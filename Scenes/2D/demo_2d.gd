22e3cf9c6bc5f19396b89332100fa0a3dfc31c3c
Friendly Cosmonaut <friendlycosmonaut@gmail.com>
2026-03-07 18:10:32 +1100

    model tweaks

--- Scenes/2D/demo_2d.gd
+++ Scenes/2D/demo_2d.gd
@@ -7,7 +7,7 @@ extends Node2D
 @onready var angle = get_node("AngleLine")
 @onready var looker = get_node("Looker")
 @onready var target = get_node("Target")
-@onready var target_label = get_node("Label")
+@onready var target_label = target.get_node("Label")
 
 func _process(delta: float) -> void:
 	var viewport = get_viewport()
@@ -19,11 +19,13 @@ func _process(delta: float) -> void:
 		target.position.x = mouse.x - (size.x/2)
 		target.position.y = mouse.y - (size.y/2)
 		looker.rotation = atan2(target.position.y, target.position.x)
+		label.text = "atan2(y, x)"
 	else:
 		## ArcTangent
 		target.position.x = mouse.x - (size.x/2)
 		target.position.y = mouse.y - (size.y/2)
 		looker.rotation = atan(target.position.y / target.position.x)
+		label.text = "atan(y / x)"
 	
 	## Update the Circle's angle hand
 	angle.rotation = looker.rotation
@@ -35,12 +37,11 @@ func update_labels():
 	## Update our labels
 	## Again, we flip the y axis for this summary text because of Godot's flipped Y axis
 	target_label.text = "x: " + str(target.position.x) + ", y: " + str(- target.position.y)
-	label.text = "x: " + str(looker.position.x) + ", y: " + str(- looker.position.y)
 	
 	var radians = Maths.round_to(- looker.rotation / PI, 1)
 	var degrees = floor(rad_to_deg(- looker.rotation))
-	label.text = ("atan2(y, x)" 
-		+ "\n= Radians: " + str(radians) + " * π" 
+	label.text = label.text + (
+		"\n= Radians: " + str(radians) + " * π" 
 		+ "\n= Degrees: " + str(degrees)
 	)
 	
